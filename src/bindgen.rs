7aeff4d0e453da95f0e204b2aeb17526fded1f5d
Catchawink <threeeasywords@gmail.com>
2025-07-02 15:33:47 -0400

    Update bindgen.rs

--- src/bindgen.rs
+++ src/bindgen.rs
@@ -332,7 +332,7 @@ pub fn run_for_file(builder: bindgen::Builder, output_file: impl AsRef<Path>) ->
 
     // Hacky fix
     let mut contents = fs::read_to_string(&output_file)?;
-    contents = contents.replace("\\u{1}", "").replace('\u{0001}', "");
+    contents = contents.replace("\\u{1}", "").replace('\u{0001}', "").replace(r"\u{1}", "");
 
     // also strip any real 0x01 bytes, just in case
     contents = contents.replace('\x01', "");

75558ba3f04d0199e75c644adcac99b6c92d5cbc
Catchawink <threeeasywords@gmail.com>
2025-07-02 13:07:36 -0400

    Update bindgen.rs

--- src/bindgen.rs
+++ src/bindgen.rs
@@ -169,6 +169,7 @@ impl Factory {
             .layout_tests(false)
             .formatter(bindgen::Formatter::None)
             .derive_default(true)
+            .trust_clang_mangling(false)
             .clang_arg("-D__bindgen")
             // Include directories provided by the build system
             // should be first on the search path (before sysroot includes),
